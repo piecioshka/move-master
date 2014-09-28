f46b7da1ebf92dad9a6d61ca8343ebbccdae64ac
Piotr Kowalski <piecioshka@gmail.com>
2014-09-28 23:03:12 +0200

    Prepare some comments about MoveMaster :yum:

--- scripts/MoveMaster.js
+++ scripts/MoveMaster.js
@@ -1,37 +1,74 @@
+// # MoveMaster.js
+//
+// Author: Piotr Kowalski
+// Contact: piecioshka@gmail.com
+// License: The MIT License
+// Date: 2014-09-28
+//
+// ### Example
+// ```js
+// var logo = document.querySelector('#logo');
+// new MoveMaster(logo);
+// ```
+
+/*global define */
+
 (function (root) {
     'use strict';
 
-    var doc = root.document;
+    /**
+     * Apply moving to any HTMLElement.
+     * Indicated element must have CSS rule `position: absolute; left: [X]; top: [X];` to be elastic on moving.
+     * Parent element must have at least `position: relative;`.
+     *
+     * @param {HTMLElement} object Indicated element to move.
+     * @param {HTMLElement} [parent = document.body] Wrapper of indicated element.
+     * @constructor
+     */
+    var MoveMaster = function (object, parent) {
+        // Protection incorrect call.
+        if (!(this instanceof MoveMaster)) {
+            throw new Error('MoveMaster: Use new operator to run MoveMaster constructor!');
+        }
 
-    var MoveMaster = function (object) {
+        // Protection object is incorrect type.
+        if (!(object instanceof root.HTMLElement)) {
+            throw new Error('MoveMaster: Expected `object` as instance of HTMLElement!');
+        }
+
         this.element = object;
+        this.parent = parent || root.document.body;
         this.x = 0;
         this.y = 0;
         this.isMove = false;
         this.left = 0;
         this.top = 0;
+
         this.initialize();
     };
 
     MoveMaster.prototype.initialize = function () {
-        var st = window.getComputedStyle(this.element, null);
+        var st = root.getComputedStyle(this.element, null);
         this.left = parseInt(st.getPropertyValue('left'), 10);
         this.top = parseInt(st.getPropertyValue('top'), 10);
 
-        doc.body.addEventListener('mousedown', this.start.bind(this));
-        doc.body.addEventListener('mousemove', this.move.bind(this));
-        doc.body.addEventListener('mouseup', this.stop.bind(this));
+        this.parent.addEventListener('mousedown', this.start.bind(this));
+        this.parent.addEventListener('mousemove', this.move.bind(this));
+        this.parent.addEventListener('mouseup', this.stop.bind(this));
     };
 
     MoveMaster.prototype.update = function (deltaX, deltaY) {
-        var newX = this.left + deltaX;
-        var newY = this.top + deltaY;
+        var newX, newY;
 
-        if (newX > 0 && newX < root.window.innerWidth - this.element.width) {
+        // Calculate new position on X and Y axis.
+        newX = this.left + deltaX;
+        newY = this.top + deltaY;
+
+        if (newX > 0 && newX < root.window.innerWidth - this.element.offsetWidth) {
             this.element.style.left = newX + 'px';
         }
 
-        if (newY > 0 && newY < root.window.innerHeight - this.element.height) {
+        if (newY > 0 && newY < root.window.innerHeight - this.element.offsetHeight) {
             this.element.style.top = newY + 'px';
         }
     };
@@ -59,6 +96,15 @@
     };
 
     // Exports `MoveMaster`.
-    return (root.MoveMaster = MoveMaster);
+    if (typeof root.define === 'function' && root.define.amd) {
+        // Support AMD style.
+        root.define(MoveMaster);
+    } else {
+        // Simple add global object.
+        root.MoveMaster = MoveMaster;
+    }
+
+    // If someone don't like if module returns `undefined`.
+    return MoveMaster;
 
 }(this));
