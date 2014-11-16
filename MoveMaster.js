b3b390efddfe97eb113aa9177bee7abbe95752d2
Piotr Kowalski <piecioshka@gmail.com>
2014-11-16 17:59:35 +0100

    Get example from Winamp2.js. Close #1

--- MoveMaster.js
+++ MoveMaster.js
@@ -19,19 +19,16 @@
 (function (root) {
     'use strict';
 
-    // Main module
-    // -----------
-
     /**
      * Apply moving to any HTMLElement.
      * Indicated element must have CSS rule `position: absolute; left: [X]; top: [X];` to be elastic on moving.
      * Parent element must have at least `position: relative;`.
      *
      * @param {HTMLElement} options.object Indicated element to move.
-     * @param {HTMLElement} options.parent Wrapper of indicated element.
-     * @param {HTMLElement} options.hook
+     * @param {HTMLElement} [options.parent] Wrapper of indicated element.
+     * @param {HTMLElement} [options.hook] Additional hooker which moves main object.
+     *
      * @constructor
-     * @throws When invalid run `MoveMaster` (without operator new).
      * @throws When params object in options object is not instance of HTMLElement.
      */
     var MoveMaster = function (options) {
@@ -41,14 +38,10 @@
 
         var element = options.object;
         var parent = options.parent || root.document.body;
-        var hook = options.hook || null;
-        var isMove = false;
+        var hook = options.hook || element;
 
         var left = 0;
         var top = 0;
-
-        var startX = 0;
-        var startY = 0;
 
         // Disable draggable.
         element.draggable = false;
@@ -65,55 +58,48 @@
             top = parseInt(st.getPropertyValue('top'), 10) || 0;
         }
 
-        // Method updating object what is moved.
-        function update(deltaX, deltaY) {
-            // Calculate new position on X and Y axis.
-            element.style.left = (left + deltaX) + 'px';
-            element.style.top = (top + deltaY) + 'px';
-        }
-
         // Handler call when user run `mousedown` event in parent element.
-        function start(evt) {
-            if (isTarget(evt)) {
-                parent.addEventListener('mousemove', move, false);
-                parent.addEventListener('mouseup', stop, false);
-
-                isMove = true;
-
-                startX = evt.clientX;
-                startY = evt.clientY;
-
-                // Update cursor above moving element.
-                evt.target.style.cursor = 'move';
+        hook.addEventListener('mousedown', function start(evt) {
+            if (!isTarget(evt)) {
+                return;
             }
-        }
 
-        // Handler call on each `mousemove` event.
-        function move(evt) {
-            if (isMove) {
-                update(evt.clientX - startX, evt.clientY - startY);
+            var mouseStartLeft = evt.clientX;
+            var mouseStartTop = evt.clientY;
+
+            parent.addEventListener('mousemove', handleMove);
+            parent.addEventListener('mouseup', stop);
+
+            // Update cursor above moving element.
+            evt.target.style.cursor = 'move';
+
+            // Handler call on each `mousemove` event.
+            function handleMove(evt) {
+                var mouseLeft = evt.clientX;
+                var mouseTop = evt.clientY;
+
+                var deltaLeft = mouseLeft - mouseStartLeft;
+                var deltaTop = mouseTop - mouseStartTop;
+
+                // Calculate new position on X and Y axis.
+                element.style.left = (left + deltaLeft) + 'px';
+                element.style.top = (top + deltaTop) + 'px';
             }
-        }
 
-        // Handler of `mouseup` event.
-        function stop(evt) {
-            parent.removeEventListener('mousemove', move, false);
-            parent.removeEventListener('mouseup', stop, false);
+            // Handler of `mouseup` event.
+            function stop(evt) {
+                parent.removeEventListener('mousemove', handleMove);
+                parent.removeEventListener('mouseup', stop);
 
-            isMove = false;
-            // Restore cursor to auto mode.
-            evt.target.style.cursor = 'auto';
+                // Restore cursor to auto mode.
+                evt.target.style.cursor = 'auto';
+            }
 
             loadPosition();
-        }
-
-        loadPosition();
-
-        parent.addEventListener('mousedown', start, false);
+        });
     };
 
     // Exports `MoveMaster`.
-    // ---------------------
 
     if (typeof root.define === 'function' && root.define.amd) {
         // Support AMD style.
